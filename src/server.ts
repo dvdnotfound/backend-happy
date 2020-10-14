f7fe568168317dbd8c4d6e5a5941aae3a2572ec9
David Santos <contato.dvdsantos@gmail.com>
2020-10-14 01:33:08 -0300

    repository

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
+app.post('/orphanages', (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
     return response.json({ message: 'Hello World'});
 });
 
