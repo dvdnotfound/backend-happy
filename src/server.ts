c6f399e4a8fc1a934d56ce75d810ab5427db408b
David Santos <contato.dvdsantos@gmail.com>
2020-10-14 02:21:36 -0300

    controllers

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/orphanages', (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
-    return response.json({ message: 'Hello World'});
-});
+app.use(routes);
 
 app.listen(3333);
 
